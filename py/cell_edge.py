bdef852ee3e18d4bd7311525bb1a81c28602113d
wisstock <omnia.fatum@gmail.com>
2020-02-06 16:16:36 +0200

    cellEdge fun

--- py/cell_edge.py
+++ py/cell_edge.py
@@ -30,12 +30,12 @@ def getTiff(file_name, channel=0, frame_number=0, camera_offset=250):
     wd_path = os.path.split(os.getcwd())
     os.chdir(wd_path[0] + '/temp/data/')
     tiff_tensor = tifffile.imread(file_name)
-    print(tiff_tensor.shape, np.max(tiff_tensor))
+    # print(tiff_tensor.shape, np.max(tiff_tensor))
 
     channel_one = tiff_tensor[0::2, :, :]
     channel_two = tiff_tensor[1::2, :, :]
-    print(channel_one.shape)
-    print(channel_two.shape)
+    # print(channel_one.shape)
+    # print(channel_two.shape)
 
     if channel == 0:
     	frame_amount = channel_one.shape
@@ -54,22 +54,10 @@ def getTiff(file_name, channel=0, frame_number=0, camera_offset=250):
     		if frame_number > frame_amount[0]:
     			print("Frame number out of range!")
 
-def cellThresh(img):
-	cell_mask = threshold_triangle(img)
-	out = img > cell_mask
-
-	return(out)
-
-def cellPercent(img, percent=80):
-	percentile = np.percentile(img, percent)
-	out = np.copy(img)
-	out[out < percentile] = 0
-
-	return(out)
-
-def cellEdge(img, thbreshold_method="triangle", seed_method="first", percent=85):
+def cellEdge(img, thbreshold_method="triangle", percent=90, seed_method="one"):
     '''
 	seed methods:
+        one - calculate threshold for one image
 	    first - build threshold mask for first frame of series
 	    max - build threshold mask for max intensity frame
 	    mean - build threshold mask for mean intensity of all serie frames
@@ -80,30 +68,40 @@ def cellEdge(img, thbreshold_method="triangle", seed_method="first", percent=85)
 
 	'''
 
-    thresh_out = threshold_triangle(img)
-    positive_mask = img > thresh_out  # create negative threshold mask
-    threshold_mask = positive_mask * -1  # inversion threshold mask
+    if thbreshold_method == "triangle":
+        thresh_out = threshold_triangle(img)
+        positive_mask = img > thresh_out  # create negative threshold mask
+        threshold_mask = positive_mask * -1  # inversion threshold mask
 
-    output_img = np.copy(img)
-    output_img[threshold_mask] = 0
+        output_img = np.copy(img)
+        output_img[threshold_mask] = 0
 
-    return(threshold_mask, output_img)
+        return(output_img)
 
+    elif thbreshold_method == "percent":
+        percentile = np.percentile(img, percent)
+        output_img = np.copy(img)
+        output_img[output_img < percentile] = 0
+
+        return(output_img)
+
+    else:
+        print("Incorrect treshold method!")
 
 
 
 input_file = 'Fluorescence_435nmDD500_cell1.tiff'
 
-img = getTiff(input_file, 0, 0)
+
+
+img = getTiff(input_file, 1, 10)
 img_eq = exposure.equalize_hist(img)
 
-img_perc = cellPercent(img, 95)
+img_perc = cellEdge(img, "percent", 97)
 img_eq_perc = exposure.equalize_hist(img_perc)
 
-mask, img_edge = cellEdge(img)
-img_eq_edge = exposure.equalize_hist(img_edge)
-
-print(mask)
+# img_edge = cellEdge(img)
+# img_eq_edge = exposure.equalize_hist(img_edge)
 
 
 
@@ -118,10 +116,10 @@ ax0.axis("off")
 ax1.imshow(img_eq)
 ax1.axis("off")
 
-ax2.imshow(mask)
+ax2.imshow(img_perc)
 ax2.axis("off")
 
-ax3.imshow(img_edge)
+ax3.imshow(img_eq_perc)
 ax3.axis("off")
 
 plt.show()
