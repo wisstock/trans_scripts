2ef5efc453bddbc03c1e16d1953f0150ce1a0756
wisstock <omnia.fatum@gmail.com>
2020-02-13 03:56:16 +0200

    lineSlice & lineExtract done

--- py/line_demo.py
+++ py/line_demo.py
@@ -54,7 +54,7 @@ def getTiff(file_name, channel=0, frame_number=0, camera_offset=250):
     		if frame_number > frame_amount[0]:
     			print("Frame number out of range!")
 
-def lineSlice(img, angle=0, cntr_coord="center"):
+def lineSlice(img, angle=1, cntr_coord="center"):
     '''
     Returns coordinates of intersection points custom line with frame edges.
     Requires cell image mass center coordinates (default set up frame center)
@@ -65,13 +65,13 @@ def lineSlice(img, angle=0, cntr_coord="center"):
                 
 
            Vertical line (v)     
-          180 
-    Up(u)  | mass centre coord
-           |/
-    90 ----+---- Horizontal line (h) 
-           |
-    Down(d)|
-           0
+           180 
+    Up(u)   | mass centre coord
+            |/
+    270 ----+---- 90 Horizontal line (h) 
+            |
+    Down(d) |
+            0
 
     y = a * x + b
     a = tg alpha
@@ -90,11 +90,11 @@ def lineSlice(img, angle=0, cntr_coord="center"):
      B| *                |
       |   *              |
       |     *            |
-      |  Up   *          |
+      |       *          |
       |        a* O      |
      A|-----------*------|A'
       |           | *    |
-      |  D        |   *  |
+      |           |   *  |
       |           |     *|B'
      S+-----------------------> x
                          X
@@ -111,16 +111,16 @@ def lineSlice(img, angle=0, cntr_coord="center"):
       y
       ^
       |
-     Y|*
-      | *
-      |  *                
-      |   *               
-      |    *C              
-     B|-----* -----------               
-      |      *           |
+     Y| *
+      |  *
+      |   *                
+      |    *               
+      |     *C              
+     B|------* ----------               
       |       *          |
       |        *         |
-      |        a* O      |
+      |         *        |
+      |         a* O     |
      A|-----------*------|A'
       |           |*     |
       |           | *    |
@@ -145,26 +145,26 @@ def lineSlice(img, angle=0, cntr_coord="center"):
         (because slice is diameter, not radius)
 
         '''
-        if 0 < angl < 90:
+        if 0 < angl < 90 or 180 < angl < 270:
             logging.debug("anglPars done! d")
-            return("d", math.radians(angl))
-        elif 90 < angl < 180:
+            return("d", math.radians(90 - angl))
+        elif 90 < angl < 180 or 270 < angl < 360:
             logging.debug("anglPars done! u")
-            return("u", math.radians(180-angl))
-        elif angl == 0 | angl == 180:
+            return("u", math.radians(angl-90))
+        elif angl == 0 or angl == 180:
             logging.debug("anglPars done! v")
-            return("v", "NaN")
-        elif angl == 90:
+            return("v", 0)
+        elif angl == 90 or angl == 270:
             logging.debug("anglPars done! h")
-            return("h", "NaN")
+            return("h", 90)
 
-    x0, y0, x1, y1 = 0, 0, 0, 0  
+    x0, y0, x1, y1 = 0, 0, 0, 0  # init line ends coordinates
     img_shape = np.shape(img)
 
     logging.debug("array shape: %s", img_shape)
 
-    x_lim = img_shape[0]-1
-    y_lim = img_shape[1]-1
+    x_lim = img_shape[1]-1  # create global image size var 
+    y_lim = img_shape[0]-1  # "-1" because pixels indexing starts from 0
 
     logging.debug("X coord lim: %s, Y coord lim: %s" % (x_lim, y_lim))
 
@@ -177,46 +177,92 @@ def lineSlice(img, angle=0, cntr_coord="center"):
 
     AO_left = cntr_coord[0]
     OA_right = x_lim - cntr_coord[0]
+
     x_cntr = cntr_coord[0]
     y_cntr = cntr_coord[1]
+    
 
     logging.debug("center coord: %s" % cntr_coord)
-    logging.debug("indicator = %s" % indicator)
 
 
     if indicator == "h":  # boundary cases check
-        x0, y0 = cntr_coord[0], 0
-        x1, y1 = cntr_coord[0], y_lim
+        x0, y0 = 0, cntr_coord[1]
+        x1, y1 = x_lim, cntr_coord[1] 
         logging.debug("coordinate h")
         logging.debug("0 point %s, 1 point %s" % ([x0, y0], [x1, y1]))
         return([x0, y0], [x1, y1])
 
     elif indicator == "v":  # boundary cases check
-        x0, y0 = x_lim, cntr_coord[1]
-        x1, y1 = 0, cntr_coord[1]
+        x0, y0 = cntr_coord[0], y_lim
+        x1, y1 = cntr_coord[0], 0
         logging.debug("coordinate v")
         logging.debug("0 point %s, 1 point %s" % ([x0, y0], [x1, y1]))
         return([x0, y0], [x1, y1])
 
-    elif indicator == "u":
+
+    elif indicator == "d":
+        AB_left = np.int(cntr_coord[0]*math.tan(angl_rad))  # AB, see on comment above
+        
+        logging.debug("AB: %s" % AB_left)
+        # logging.debug("BO: %s" % BC_left)
+
         # calculate up left (90-180)
-        AB_left = np.int(cntr_coord[0]*math.tan(angl_rad))
-        if  AB_left <= y_lim - cntr_coord[1]:
-            logging.debug("AB left: %s" % AB_left)
+        if  cntr_coord[1] - AB_left > 0:
             x0 = 0
-            y0 = cntr_coord[1] + AB_left
-        elif AB_left > y_lim - cntr_coord[1]:
-            x0 = cntr_coord[0] - np.int((y_lim - cntr_coord[1])/math.tan(angl_rad))
-            y0 = y_lim
+            y0 = cntr_coord[1] - AB_left
+            logging.debug("AB > 0. x0, y0 = %s, %s" % (x0, y0))
+        elif cntr_coord[1] - AB_left <= 0:
+            BO_left = np.int(cntr_coord[1]/math.tan(angl_rad))  # BO, see comment above
+            x0 = cntr_coord[0] - BO_left
+            y0 = 0
+            logging.debug("AB < y max. x0, y0 = %s, %s" % (x0, y0))
+
+        AB_right = np.int((x_lim - cntr_coord[0])*math.tan(angl_rad))  # A'B', see comment above
+        logging.debug("A'B': %s" % AB_right)
+        # logging.debug("B'C': %s" % BC_right)
 
         # calculate up right
-        AB_right = np.int((x_lim - cntr_coord[0])*math.tan(angl_rad))
-        if cntr_coord[1] - AB_right >= 0:
+        if AB_right < y_lim - cntr_coord[1]:
+            x1 = x_lim
+            y1 = cntr_coord[1] + AB_right
+            logging.debug("A'B' > y_max. x1, y1 = %s, %s" % (x1, y1))
+        elif AB_right >= y_lim - cntr_coord[1]:
+            BC_right = ((x_lim - cntr_coord[0]) - np.int((y_lim - cntr_coord[1])/math.tan(angl_rad)))  # B'C', see comment above
+            x1 = x_lim - BC_right
+            y1 = y_lim
+            logging.debug("A'B' > y max. x1, y1 = %s, %s" % (x1, y1))
+
+
+    elif indicator == "u":
+        AB_left = np.int(cntr_coord[0]*math.tan(angl_rad))  # AB, see comment above
+        logging.debug("AB: %s" % AB_left)
+        # logging.debug("BO: %s" % BC_left)
+
+        # calculate up left (90-180)
+        if  AB_left < y_lim - cntr_coord[1]:
+            x0 = 0
+            y0 = cntr_coord[1] + AB_left
+            logging.debug("AB < y max. x0, y0 = %s, %s" % (x0, y0))
+        elif AB_left >= y_lim - cntr_coord[1]:
+            BO_left = np.int((y_lim - cntr_coord[1])/math.tan(angl_rad))  # BC, see comment above
+            x0 = cntr_coord[0] - BC_left
+            y0 = y_lim
+            logging.debug("AB > y max. x0, y0 = %s, %s" % (x0, y0))
+
+        AB_right = np.int((x_lim - cntr_coord[0])*math.tan(angl_rad))  # A'B', see comment above
+        logging.debug("A'B': %s" % AB_right)
+        # logging.debug("B'C': %s" % BC_right)
+
+        # calculate up right
+        if cntr_coord[1] - AB_right > 0:
             x1 = x_lim
             y1 = cntr_coord[1] - AB_right
-        elif cntr_coord[1] - AB_right < 0:
-            x1 = x_lim  - np.int(cntr_coord[1]/math.tan(angl_rad))
+            logging.debug("A'B' > 0. x1, y1 = %s, %s" % (x1, y1))
+        elif cntr_coord[1] - AB_right <= 0:
+            BC_right = (cntr_coord[0] - np.int(cntr_coord[1]/math.tan(angl_rad)))  # B'C', see comment above
+            x1 = x_lim - BC_right
             y1 = 0
+            logging.debug("A'B' < 0. x1, y1 = %s, %s" % (x1, y1))
 
     return([x0, y0], [x1, y1])
 
@@ -229,8 +275,8 @@ def lineExtract(img, start_coors = [0, 0], end_coord = [100, 100]):
 
     logging.debug("start point, end point: %s, %s" % (start_coors, end_coord))
 
-    x0, y0 = start_coors[0], start_coors[1]
-    x1, y1 = end_coord[0], end_coord[1]
+    x0, y0 = start_coors[1], start_coors[0]
+    x1, y1 = end_coord[1], end_coord[0]
     line_length = int(np.hypot(x1-x0, y1-y0))  # calculate line length
 
     logging.debug("line length: %s" % (np.hypot(x1-x0, y1-y0)))
@@ -249,29 +295,31 @@ def lineExtract(img, start_coors = [0, 0], end_coord = [100, 100]):
 
 
 input_file = 'Fluorescence_435nmDD500_cell1.tiff'
-angle = 120
-
+angle = 270
 
 img = getTiff(input_file, 1, 10)
 # data_shape = img.shape()
 # print(data_shape[1], data_shape[2])
 
-start, end = lineSlice(img, angle)
-print(start, end)
-# line_slice = lineExtract(img, start, end)
+cntr = [250,800]
+
+start, end = lineSlice(img, angle, cntr)
+line_slice = lineExtract(img, start, end)
 shape = np.shape(img)
-cntr = [np.int((shape[0]-1)/2),
-        np.int((shape[1]-1)/2)]
-print(cntr)
+# cntr = [np.int((shape[1]-1)/2),
+        # np.int((shape[0]-1)/2)]
 
 
-fig, (ax0) = plt.subplots(nrows=1,
+fig, (ax0, ax1) = plt.subplots(nrows=2,
                           ncols=1,
                           figsize=(8, 8))
 
 ax0.imshow(img)  #, cmap='gray')
-ax0.plot([start[1], end[1]], [start[0], end[0]], 'ro-')
-ax0.scatter(cntr[1],cntr[0],color='r')
+ax0.plot([start[0], end[0]], [start[1], end[1]], 'ro-')
+ax0.scatter(cntr[0],cntr[1],color='r')
+# ax0.scatter(start[0]+5, start[1]+5)
 
-plt.gca().invert_yaxis()
+ax1.plot(line_slice)
+
+# plt.gca().invert_yaxis()
 plt.show()
